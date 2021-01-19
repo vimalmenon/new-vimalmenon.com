79c18e8d2b628aedaa17388d33117db67ba8d6ce
Vimal Menon <69777626+vimalsmenon@users.noreply.github.com>
2021-01-19 08:45:01 +0800

    Working on Tutorial font size

--- assets/model/page/announcements/index.ts
+++ assets/model/page/announcements/index.ts
@@ -25,19 +25,7 @@ const announcements = {
 			date:"Jan 16, 2021",
 			description:"Check out my new video on HTML form and form elements",
 			link:""
-		},
-		{
-			title:"New Youtube Video Release",
-			date:"Jan 16, 2021",
-			description:"Check out my new video on HTML form and form elements",
-			link:""
-		},
-		{
-			title:"New version released",
-			date:"Jan 16, 2021",
-			description:"New version (v0.0.3.a) of website released",
-			link:""
-		},
+		}
 	]
 };
 
